32e427f3a6ec6369727d3e44b5d0f4d77edc8ec7
dev-tam <tamjid.mdh@gmail.com>
2017-12-11 07:25:05 +0600

    Initial commit

---
+++ app/src/main/java/net/torora/jtam/zoo/Animal.kt
@@ -0,0 +1,16 @@
+package net.torora.jtam.zoo
+
+/**
+ * Created by jtam on 12/11/17.
+ */
+class Animal{
+    var name:String?=null
+    var des:String?=null
+    var image:Int?=null
+
+    constructor(name:String, des:String, image:Int){
+        this.name=name
+        this.des=des
+        this.image=image
+    }
+}
