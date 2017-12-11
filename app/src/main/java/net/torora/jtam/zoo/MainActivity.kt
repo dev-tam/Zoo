3fe74b73c22c4faefe4420590e91ed0329f6668d
dev-tam <tamjid.mdh@gmail.com>
2017-12-11 08:15:02 +0600

    loaded data to listview

--- app/src/main/java/net/torora/jtam/zoo/MainActivity.kt
+++ app/src/main/java/net/torora/jtam/zoo/MainActivity.kt
@@ -1,12 +1,61 @@
 package net.torora.jtam.zoo
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.animal_ticket.view.*
 
 class MainActivity : AppCompatActivity() {
 
+    var listOfAnimals = ArrayList<Animal>()
+    var adapter:AnimalsAdapter?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // load Animals
+        listOfAnimals.add(Animal("Baboon","Id at sagittis et, pretium consequat.",R.drawable.baboon))
+        listOfAnimals.add(Animal("Bulldog","Id at sagittis et, pretium consequat.",R.drawable.bulldog))
+        listOfAnimals.add(Animal("Panda","Id at sagittis et, pretium consequat.",R.drawable.panda))
+        listOfAnimals.add(Animal("Swallow Bird","Id at sagittis et, pretium consequat.",R.drawable.swallow_bird))
+        listOfAnimals.add(Animal("White Tiger","Id at sagittis et, pretium consequat.",R.drawable.white_tiger))
+        listOfAnimals.add(Animal("Zebra","Id at sagittis et, pretium consequat.",R.drawable.zebra))
+
+        adapter = AnimalsAdapter(this,listOfAnimals)
+        tvListAnimal.adapter=adapter
+    }
+    class AnimalsAdapter:BaseAdapter{
+        var listOfAnimals = ArrayList<Animal>()
+        var context:Context?=null
+        constructor(context: Context, listOfAnimals: ArrayList<Animal>):super(){
+            this.listOfAnimals=listOfAnimals
+            this.context=context
+        }
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val animal = listOfAnimals[position]
+            var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            var myView = inflator.inflate(R.layout.animal_ticket,null)
+            myView.tvName.text=animal.name!!
+            myView.tvDes.text=animal.des!!
+            myView.ivAnimalImage.setImageResource(animal.image!!)
+            return myView
+        }
+
+        override fun getItem(position: Int): Any {
+            return listOfAnimals[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return listOfAnimals.size
+        }
+
     }
 }
